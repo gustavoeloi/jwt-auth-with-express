9d4e00648ef97c86d1e010ce6cc7e2f8d7fd1d0a
gustavooeloi <gustavooeloi@gmail.com>
2026-01-05 13:43:37 -0300

    first commit

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,9 +1,13 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 
 class ProductsController {
-  async index(request: Request, response: Response) {}
+  async index(request: Request, response: Response) {
+    return response.json({ message: "Ok" });
+  }
 
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    return response.json({ message: "OK" });
+  }
 }
 
-export { ProductsController }
+export { ProductsController };
